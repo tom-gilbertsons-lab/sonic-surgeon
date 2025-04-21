9ff3b231f97d2d8f18bee3fe335fd4950da8afc7
Isla Barnard <islarosemarybarnard@gmail.com>
2025-04-21 10:24:56 +0100

    nice tap effect

---
+++ Assets/Scripts/OnTapTreat.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class OnTapTreat : MonoBehaviour
+{
+    public float pulseDuration = 1.3f;
+
+    private Material mat;
+    private Coroutine pulseRoutine;
+
+    void Start()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+        mat.SetFloat("_PulseProgress", 0f);
+    }
+
+    void OnMouseDown()
+    {
+        Vector3 worldClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 localClick = transform.InverseTransformPoint(worldClick);
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        Debug.Log("click on SG");
+
+        float uvX = (localClick.x / sr.sprite.bounds.size.x) + 0.5f;
+        float uvY = (localClick.y / sr.sprite.bounds.size.y) + 0.5f;
+
+        mat.SetVector("_TapPoint", new Vector4(uvX, uvY, 0, 0));
+
+        // mat.SetFloat("_PulseProgress", 0.8f);
+
+        Debug.Log("clicked on slice");
+        //mat.SetVector("_TapPoint", new Vector4(0.5f, 0.5f, 0, 0));
+
+
+
+        //if (pulseRoutine != null) StopCoroutine(pulseRoutine);
+        pulseRoutine = StartCoroutine(AnimatePulse());
+    }
+
+    System.Collections.IEnumerator AnimatePulse()
+    {
+        float t = 0f;
+
+
+        while (t < pulseDuration)
+        {
+
+            float progress = t / pulseDuration;
+
+            mat.SetFloat("_HeatProgress", progress);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        mat.SetFloat("_HeatProgress", 0f);
+    }
+}
